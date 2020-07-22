6fb8da86563737b84127cb3fb1aff1f0b002debb
Chris Petty <cpetty@enve.com>
2020-07-22 13:02:56 -0600

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,15 @@
+const request = require('postman-request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=12dfb5cd387cd005e3dee7dd402a01ec&query=' + latitude + ',' + longitude + '&units=f'
+    request ({url, json:true},(error,{body}) =>{
+        if (error) {
+            callback('Unable to get connection to site', undefined)
+        } else if(body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+        }
+    } )
+}
+module.exports = forecast
